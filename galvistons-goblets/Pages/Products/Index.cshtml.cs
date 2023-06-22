be81837f2ce75791309d823e70c28b76bf8c6cea
shawn-coding <shawncode484@gmail.com>
2023-06-21 21:50:13 -0400

    Initial commit of dotnet core project

---
+++ galvistons-goblets/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace galvistons_goblets.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
